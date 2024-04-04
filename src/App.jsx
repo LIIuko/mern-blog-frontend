018e2d5a83d2d50185b6eb7300fc9f53fd213c35
Konstantin <doro.kon@mail.ru>
2024-04-04 18:00:51 +0300

    Added structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import Container from "@mui/material/Container";
+
+import Header from "./components/Header/index.jsx";
+import {Home} from "./pages/index.js";
 
 const App = () => {
     return (
         <>
-
+            <Header/>
+            <Container maxWidth="lg">
+                <Home />
+            </Container>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <CssBaseline/>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
 )
